6a62249448fb6672f216bdfed96db1a6397c8e6e
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-19 14:24:43 -0500

    REWIND Atlas
    first save

--- js/site.js
+++ js/site.js
@@ -1,14 +1,42 @@
 
 
 
-// display message to the user.
+// PULL USER INPUT FROM PAGE
 
-function getMessage() {
-    let userMessage = document.getElementById("message").value;
+function getValues() {
+    let userString = document.getElementById("reverseString").value;
+
+    //  CALL REVERSE A STRING ------
+    reverseAString(userString);
+    let revString = reverseAString(userString);
+
+    // DISPLAY THE REVERSED STRING ------
+    displayData(revString);
+
     Swal.fire(
         'Button clicked!',
         userMessage,
         "success"
     );
-    console.log(userMessage);
+}
+
+// -------------------THIS GENERATES DATA -------------
+function reverseAString (userString) {
+
+    let revString = "";
+
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+
+    return revString;
+}
+
+// ------------------------ DISPLAY THE REVERSED STRING TO PAGE --------
+function displayData (revString) {
+
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    document.getElementById("alert").classList.remove("d-none");
+
 }
